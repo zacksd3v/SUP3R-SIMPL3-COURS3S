0659fdc55faa81d9c890f9a1372f82fa8c3dc381
zacksd3v <zacksd3v@gmail.com>
2026-04-12 14:33:42 +0100

    Using Selector to add Items

---
+++ JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson13/1/javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,137 @@
+// const products = [{
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//         stars: 4.5,
+//         count: 87
+//     }, 
+//     priceCents: 1090 
+// }, {
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: ' Intermediate Size Basketball',
+//     rating: {
+//         stars: 4,
+//         count: 127
+//     },
+//     priceCents: 2095
+// }, {
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 4.5,
+//         count: 56
+//     },
+//     priceCents: 799
+// }, {
+//     image: 'images/products/black-2-slot-toaster.jpg',
+//     name: '2 Slot Toaster - Black',
+//     rating: {
+//         stars: 5,
+//         count: 2197
+//     },
+//     priceCents: 1899
+// }];
+
+let productsHtml = '';
+
+//// Exercise Lesson 13a = find the selector tag.
+//// Exercise Lesson 13b = add a class & id.
+products.forEach((product) => { 
+    productsHtml += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+    `;
+});
+
+document.querySelector('.js-product-grid')
+    .innerHTML = productsHtml;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            let matchingItem; 
+            let itemValue;
+
+             /// Exercise Lesson 13c = use DOM to get the class.
+            const addItemsUsingSelect = document.querySelector(`.js-quantity-selector-${productId}`);
+              itemValue = Number(addItemsUsingSelect.value); /// Exercise Lesson 13d = get the value using .value property. && Ex13e = Convert from String - No
+            
+              console.log(itemValue);
+
+            cart.forEach((item) => {
+              if (productId === item.productId) {
+                matchingItem = item;
+              }
+            });
+
+            if (matchingItem) {
+              matchingItem.quantity = itemValue;
+            } else {
+                cart.push({
+                productId: productId,
+                quantity: itemValue
+            });
+            }
+
+            let cartQuantity = 0;
+
+            // cart.forEach((item) => {
+            //   cartQuantity += item.quantity;
+            // });
+
+            // Basket (add-to-cart)
+            document.querySelector('.js-cart-quantity')
+              .innerHTML = itemValue;            
+
+            console.log(cartQuantity);
+            console.log(cart);
+        });
+    });
