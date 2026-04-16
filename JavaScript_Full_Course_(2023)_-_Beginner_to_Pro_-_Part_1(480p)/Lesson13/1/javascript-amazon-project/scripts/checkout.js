1736c543fb6ddb6a2ef637004343883f5cd7e288
zacksd3v <zacksd3v@gmail.com>
2026-04-16 14:43:00 +0100

    removing HTML element from the page

--- JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson13/1/javascript-amazon-project/scripts/checkout.js
+++ JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson13/1/javascript-amazon-project/scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
     });
     
     cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -105,5 +105,8 @@ document.querySelector('.js-order-summary')
             link.addEventListener('click', () => {
                 const productId = link.dataset.deleteId;
                     removeFromCart(productId);
+                
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+                container.remove() // zamu iya removing kowane irin element da wnn fxn dn.
             });
         });
