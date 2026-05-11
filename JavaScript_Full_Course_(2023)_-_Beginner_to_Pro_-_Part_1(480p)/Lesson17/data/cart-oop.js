e0e03dd33262b6ce53a3a7051672493197c3158d
zacksd3v <zacksd3v@gmail.com>
2026-05-11 07:57:49 +0100

    OOP class

--- JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson17/data/cart-oop.js
+++ JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson17/data/cart-oop.js
@@ -1,10 +1,12 @@
-import { renderCheckOutHeader } from "../scripts/checkout/checkOutHeader.js";
+import { renderCheckOutHeader } from "../../Lesson13/1/javascript-amazon-project/scripts/checkout/checkOutHeader.js";
 
+
+// FIRST WAY TO CREATE AN OOP
 const cart = {
   cartItems: undefined,
 
   loadFromLocalStorage() {
-  cart.cartItems = JSON.parse(localStorage.getItem('cart')) || [
+  this.cartItems = JSON.parse(localStorage.getItem('cart-oop')) || [
     {
       productId: 'zrng10042026-cont00n3nt41-rolexWatch2026',
       quantity: 2,
@@ -19,7 +21,7 @@ const cart = {
 },
 
 savingToStorage() {
-  localStorage.setItem('cart', JSON.stringify(cart.cartItems));
+  localStorage.setItem('cart-oop', JSON.stringify(this.cartItems));
 },
 
 _addToCart(productId) {
@@ -28,9 +30,9 @@ _addToCart(productId) {
        let itemValue;
 
       const addItemsUsingSelect = document.querySelector(`.js-quantity-selector-${productId}`);
-      itemValue = Number(addItemsUsingSelect.value);
+      itemValue =  addItemsUsingSelect ? addNumber(addItemsUsingSelect.value) : 1;
 
-      cart.forEach((cartItem) => {
+      this.cartItems.forEach((cartItem) => {
         if (productId === cartItem.productId) {
           matchingItem = cartItem;
         }
@@ -39,37 +41,196 @@ _addToCart(productId) {
       if (matchingItem) {
         matchingItem.quantity += itemValue;
       } else {
-          cart.push({
+          this.cartItems.push({
           productId,
           quantity: itemValue,
           deliveryOptionId: '1'
       });
       }
 
-      cart.savingToStorage();
+      this.savingToStorage();
   },
 
 removeFromCart(productId) {
     const newCart = [];
 
-    cart.cartItems.forEach((cartItem) => {
+    this.cartItems.forEach((cartItem) => {
       if (cartItem.productId !== productId) {
         newCart.push(cartItem);
       }
     });
 
-    cart.cartItems = newCart;
+    this.cartItems = newCart;
 
-    savingToStorage();
+    this.savingToStorage();
     renderCheckOutHeader();
   }
 
 
 };
 
+// SECOND WAY TO CREATE ANOTHER OOP.
+// USING COPY & PASTE WHICH IS !RECOMMENDED!
+const businessCart = {
+  cartItems: undefined,
+
+  loadFromLocalStorage() {
+  this.cartItems = JSON.parse(localStorage.getItem('business-cart')) || [
+    {
+      productId: 'zrng10042026-cont00n3nt41-rolexWatch2026',
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: '58b4fc92-e98c-42aa-8c55-b6b79996769a',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];
+},
+
+savingToStorage() {
+  localStorage.setItem('business-cart', JSON.stringify(this.cartItems));
+},
+
+_addToCart(productId) {
+      let matchingItem; 
+
+       let itemValue;
+
+      const addItemsUsingSelect = document.querySelector(`.js-quantity-selector-${productId}`);
+      itemValue =  addItemsUsingSelect ? addNumber(addItemsUsingSelect.value) : 1;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += itemValue;
+      } else {
+          this.cartItems.push({
+          productId,
+          quantity: itemValue,
+          deliveryOptionId: '1'
+      });
+      }
+
+      this.savingToStorage();
+  },
+
+removeFromCart(productId) {
+    const newCart = [];
+
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+
+    this.savingToStorage();
+    renderCheckOutHeader();
+  }
+
+
+};
+
+
+// THIRD HANYA NA CREATING OOP
+// UNSING FUNCTION LET'S SEE HOW THAT'S WORK!
+function Cart(cartsKey) {
+    const businessCart = {
+      cartItems: undefined,
+
+      loadFromLocalStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(cartsKey)) || [
+        {
+          productId: 'zrng10042026-cont00n3nt41-rolexWatch2026',
+          quantity: 2,
+          deliveryOptionId: '1'
+        },
+        {
+          productId: '58b4fc92-e98c-42aa-8c55-b6b79996769a',
+          quantity: 1,
+          deliveryOptionId: '2'
+        }
+      ];
+    },
+
+    savingToStorage() {
+      localStorage.setItem(cartsKey, JSON.stringify(this.cartItems));
+    },
+
+    _addToCart(productId) {
+          let matchingItem; 
+
+          let itemValue;
+
+          const addItemsUsingSelect = document.querySelector(`.js-quantity-selector-${productId}`);
+          itemValue =  addItemsUsingSelect ? addNumber(addItemsUsingSelect.value) : 1;
+
+          this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+              matchingItem = cartItem;
+            }
+          });
+
+          if (matchingItem) {
+            matchingItem.quantity += itemValue;
+          } else {
+              this.cartItems.push({
+              productId,
+              quantity: itemValue,
+              deliveryOptionId: '1'
+          });
+          }
+
+          this.savingToStorage();
+      },
+
+    removeFromCart(productId) {
+        const newCart = [];
+
+        this.cartItems.forEach((cartItem) => {
+          if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+          }
+        });
+
+        this.cartItems = newCart;
+
+        this.savingToStorage();
+        renderCheckOutHeader();
+      }
+
+
+  };
+
+  return businessCart;
+}
+
+
+// Code for method 3
+const cart3 = Cart('business-cart');
+const businessCart3 = Cart('business-cart');
+
 cart.loadFromLocalStorage();
+businessCart.loadFromLocalStorage();
+
+// 3rd Methods
+cart3.loadFromLocalStorage();
+businessCart3.loadFromLocalStorage();
+
+cart._addToCart('zrng21-042026-cont00n3nt41-game2026');
 
 console.log(cart);
+console.log(businessCart);
+
+console.log(cart3);
+console.log(businessCart3);
 
 // export function updateQuantity(productId, newQuantity) {
 //   if (newQuantity < 0 || newQuantity >= 1000) {
