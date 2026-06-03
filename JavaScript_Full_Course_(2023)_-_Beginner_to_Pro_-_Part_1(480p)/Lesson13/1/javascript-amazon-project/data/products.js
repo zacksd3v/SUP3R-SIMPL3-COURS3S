c272ed5b9c11059c9c6a4a91269d47dff62cf50f
zacksd3v <zacksd3v@gmail.com>
2026-06-03 08:53:49 +0100

    Inheritance

--- JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson13/1/javascript-amazon-project/data/products.js
+++ JavaScript_Full_Course_(2023)_-_Beginner_to_Pro_-_Part_1(480p)/Lesson13/1/javascript-amazon-project/data/products.js
@@ -11,7 +11,7 @@ class Product {
   // WANN HANYAR TANA DA WAHALA.
   // SABODA EACH OBJECT YANA BUQATAR NA MASA HAKA.
   // SO AS A PROGRAMMER INA BUQATAR TUNANI OUTSIDE THE BOX.
-  // SABODA HAKA ZAN AIKI DA map()
+  // SABODA HAKA ZAN AIKI DA .map()
   constructor(productDetails) {
     this.id = productDetails.id;
     this.image = productDetails.image;
@@ -31,6 +31,8 @@ class Product {
 
 }
 
+// FOR EACH PRODUCT REQUIRE WNN
+// AMMMA ZANYI AMFANI DA MAP (.map())
 const product1 = new Product({
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -48,7 +50,54 @@ const product1 = new Product({
   },);
 console.log(product1);
 
+// //// ////   INHERITANCE // /// //////// ///
+// SHINE MAGANCE CUTAR COPY&PASTE
+// ZAMU GAJI KOMAI DA PARENT YAKE DA SHI HARMA MU QARA DA ABINDA PARENT BASHI DA SHI WA CHILD
+// TAMBAYAR ITACE SHIN PARENT YANA DA ACCESS DA CHILD PROPERTIES | METHOD WANDA SHI PARENT BASHI DA SHI??? SEARCH!......
+// IN SHORT INHERITANCE YANA BAMU DAMA MUYI REUSED NA CODE B/W CLASSES.
+// DUK LOKACIN DA ZAMU YI AIKI DA INHERITENCE DOLE NE MU QARA SPECIAL KEYWRD CALLED: 'extends'
+class Clothing extends Product {
+  // IDAN ZANU QARA WANI PROPERTY YA ZAMUYI. WATCH OUT!
+  sizeChartLink;
 
+  constructor(productDetails) {
+    // BY DEFAULT CHILD CLASS YANA AMFANI DA PARENT CONTRUCTOR
+    // AMMA IDAN MUKA ZO QARA WANI PROPERTY KO METHOD NB: DOLE
+    // NE MUYI AMFANI DA SPECIAL METHOD CALLED super().
+    super(productDetails)
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+}
+
+console.log('====== INHERITANCE =START HERE !=====');
+const tShirt = new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  });
+
+  console.log(tShirt);
+  console.log(tShirt.getPrice());
+  console.log(tShirt.getStartsUrl());
+
+
+// A NAN NAYI AMFANI DA MAP(.map())
+// SABODA INA SON LOOPING NA DUKA OBJECT DIN.
+// SNN DUK BAYAN WANI LOOPIN 1 INA SON NAYI CREATING SABON OBJECT
+// BEST SOLUTION SHINE MAP.
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -825,6 +874,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
